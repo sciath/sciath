6521e7f492cacc5ad328d0180d279b262ae46b25
Dave May <dave.mayhem23@gmail.com>
2016-04-28 10:28:31 +0200

    * Added example 1 with standard C code

---
+++ example1/pth_example1.py
@@ -0,0 +1,49 @@
+
+import os
+import pth_test as pth
+import pth_batch as batch
+
+def test1():
+  
+  ranks = 1
+  
+  launch = './ex1'
+  
+  expected_file = 'ex1.expected'
+  
+  def comparefunc(unittest):
+    
+    key = '\$cputime'
+    unittest.compareFloatingPoint(key,0.01)
+    
+    key = '\$residuals'
+    unittest.compareFloatingPoint(key,0.000001)
+    
+    key = '\$kspits'
+    unittest.compareInteger(key,0)
+
+    key = '\$norm'
+    unittest.compareFloatingPoint(key,0.01)
+    
+    key = '\$rms'
+    unittest.compareFloatingPoint(key,0.01)
+  
+  # Create unit test object
+  ex1 = pth.UnitTest('ex1',ranks,launch,expected_file)
+  ex1.setVerifyMethod(comparefunc)
+  ex1.appendKeywords('@')
+  
+  return(ex1)
+
+def run_unittests_example1():
+  
+  registered_tests = [ test1() ]
+
+  os.system('gcc -o ex1 ex1.c')
+
+  launcher = batch.zpthBatchQueuingSystem()
+  launcher.executeTestSuite(registered_tests)
+  launcher.verifyTestSuite(registered_tests)
+
+if __name__ == "__main__":
+  run_unittests_example1()
