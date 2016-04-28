2c7f710ad835bd080c171bc0c1f37ed1b94de2d2
Dave May <dave.mayhem23@gmail.com>
2016-04-28 11:34:20 +0200

    * added test using output file other than stdout for comparison purposes

--- example1/pth_example1.py
+++ example1/pth_example1.py
@@ -35,11 +35,30 @@ def test1():
   
   return(ex1)
 
+def test2():
+  
+  ranks = 1
+  launch = './ex2'
+  expected_file = 'ex2.expected'
+  
+  def comparefunc(unittest):
+    key = 'Residuals'
+    unittest.compareFloatingPoint(key,0.0001)
+  
+  # Create unit test object
+  test = pth.UnitTest('ex2',ranks,launch,expected_file)
+  test.setVerifyMethod(comparefunc)
+  test.appendKeywords('@')
+  test.setComparisonFile('ex2-residual.log')
+
+  return(test)
+
 def run_unittests_example1():
   
-  registered_tests = [ test1() ]
+  registered_tests = [ test1() , test2() ]
 
   os.system('gcc -o ex1 ex1.c')
+  os.system('gcc -o ex2 ex2.c')
 
   launcher = batch.zpthBatchQueuingSystem()
   launcher.executeTestSuite(registered_tests)
