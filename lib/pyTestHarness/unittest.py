f9d316eeddf7e5f8964c91d5252dd2cc97f0e8ca
Patrick Sanan <patrick.sanan@gmail.com>
2017-07-30 09:01:59 +0200

    Unittest (deprecated) module: fix comment typo

--- lib/pyTestHarness/unittest.py
+++ lib/pyTestHarness/unittest.py
@@ -1,4 +1,4 @@
-# This module is deprecated. It is included only for backwards compatiblity.
+# This module is deprecated. It is included only for backwards compatibility.
 # Don't use it!
 import pyTestHarness.test as pthtest
 pthUnitTest = pthtest.Test
