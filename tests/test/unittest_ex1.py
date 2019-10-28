0bd3a6e6db631fa1b5be12c6a33cba083778280c
Dave May <dave.mayhem23@gmail.com>
2019-10-28 08:37:26 +0000

    Test unit test: Fixed indentation

--- tests/test/unittest_ex1.py
+++ tests/test/unittest_ex1.py
@@ -51,54 +51,54 @@ def test4(): # result: pass
 
 
 def test1_ud(): # result: pass
-  cmd = ['sh' , 'write_test1_ud.sh' ]
+    cmd = ['sh' , 'write_test1_ud.sh' ]
     
-  t = Test( Job(cmd), 'Test_1_ud', path = OUTPUT_PATH )
-  print(t.output_path)
-  t.verifier = VerifierUnixDiff(t, "./expected/t1.expected")
-  
-  job_launcher.submitJob( t.job, path = t.output_path )
-  t.verify()
-  t.print()
-  return t
+    t = Test( Job(cmd), 'Test_1_ud', path = OUTPUT_PATH )
+    print(t.output_path)
+    t.verifier = VerifierUnixDiff(t, "./expected/t1.expected")
+
+    job_launcher.submitJob( t.job, path = t.output_path )
+    t.verify()
+    t.print()
+    return t
 
 def test2_ud(): # result: fail
-  cmd = ['sh' , 'write_test2_ud.sh' ]
-  
-  t = Test( Job(cmd), 'Test_2_ud', path = OUTPUT_PATH )
-  t.verifier = VerifierUnixDiff(t, "./expected/t1.expected" )
-  
-  job_launcher.submitJob( t.job, path = t.output_path )
-  t.verify()
-  t.print()
-  return t
+    cmd = ['sh' , 'write_test2_ud.sh' ]
+    
+    t = Test( Job(cmd), 'Test_2_ud', path = OUTPUT_PATH )
+    t.verifier = VerifierUnixDiff(t, "./expected/t1.expected" )
+    
+    job_launcher.submitJob( t.job, path = t.output_path )
+    t.verify()
+    t.print()
+    return t
 
 
 
 def main():
-  try:
-    os.mkdir(OUTPUT_PATH)
-  except:
-    pass
+    try:
+        os.mkdir(OUTPUT_PATH)
+    except:
+        pass
   
-  # test using default verifierr
-  t1 = test1()
-  t2 = test2()
-  t3 = test3()
+    # test using default verifierr
+    t1 = test1()
+    t2 = test2()
+    t3 = test3()
 
-  # test using unix-diff verifierr
-  t1_ud = test1_ud()
-  t2_ud = test2_ud()
+    # test using unix-diff verifierr
+    t1_ud = test1_ud()
+    t2_ud = test2_ud()
   
-  # test with staged submit/verify
-  t4 = test4()
-  t4.verify()
-  t4.print()
-  job_launcher.clean(t4.job, path = t4.output_path)
+    # test with staged submit/verify
+    t4 = test4()
+    t4.verify()
+    t4.print()
+    job_launcher.clean(t4.job, path = t4.output_path)
 
-  tests = [t1,t2,t3,t4 , t1_ud,t2_ud]
-  for t in tests:
-    job_launcher.clean(t.job, path = t.output_path)
+    tests = [t1,t2,t3,t4 , t1_ud,t2_ud]
+    for t in tests:
+        job_launcher.clean(t.job, path = t.output_path)
 
 
 main()
