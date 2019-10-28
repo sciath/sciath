adae4a8f3cdd761bd9fc055ea2d405a82f80f5ce
Dave May <dave.mayhem23@gmail.com>
2019-10-28 08:35:42 +0000

    Test unit test: Added example of staged execute / verify. * Improved usage of output_path

--- tests/test/unittest_ex1.py
+++ tests/test/unittest_ex1.py
@@ -19,8 +19,8 @@ def test1(): # result: pass
     cmd = ['echo' , '"aBc";' , 'echo' '"kspits=30"' , 'echo ""' ]
   
     t = Test( Job(cmd), 'Test_1', path = OUTPUT_PATH )
-    job_launcher.submitJob( t.job, path = OUTPUT_PATH )
-    t.verifier.execute()
+    job_launcher.submitJob( t.job, path = t.output_path )
+    t.verify()
     t.print()
     return t
 
@@ -28,8 +28,8 @@ def test2(): # result: pass
     cmd = ['echo' , '"aBc";' , 'echo' '"kspits=30"' ]
     
     t = Test( Job(cmd), 'Test_2', path = OUTPUT_PATH )
-    job_launcher.submitJob( t.job, path = OUTPUT_PATH )
-    t.verifier.execute()
+    job_launcher.submitJob( t.job, path = t.output_path )
+    t.verify()
     t.print()
     return t
 
@@ -37,10 +37,18 @@ def test3(): # result: fail
     cmd = ['echo' , '"aBc";' , 'echo' '"kspits=30"' ]
     
     t = Test( Job(cmd,exitCode = 1), 'Test_3', path = OUTPUT_PATH )
-    job_launcher.submitJob( t.job, path = OUTPUT_PATH )
-    t.verifier.execute()
+    job_launcher.submitJob( t.job, path = t.output_path )
+    t.verify()
     t.print()
     return t
+
+def test4(): # result: pass
+    cmd = ['echo' , '"aBc";' , 'echo' '"kspits=30"' ]
+    
+    t = Test( Job(cmd), 'Test_4', path = OUTPUT_PATH )
+    job_launcher.submitJob( t.job, path = t.output_path )
+    return t
+
 
 def test1_ud(): # result: pass
   cmd = ['sh' , 'write_test1_ud.sh' ]
@@ -49,8 +57,8 @@ def test1_ud(): # result: pass
   print(t.output_path)
   t.verifier = VerifierUnixDiff(t, "./expected/t1.expected")
   
-  job_launcher.submitJob( t.job, path = OUTPUT_PATH )
-  t.verifier.execute()
+  job_launcher.submitJob( t.job, path = t.output_path )
+  t.verify()
   t.print()
   return t
 
@@ -60,26 +68,37 @@ def test2_ud(): # result: fail
   t = Test( Job(cmd), 'Test_2_ud', path = OUTPUT_PATH )
   t.verifier = VerifierUnixDiff(t, "./expected/t1.expected" )
   
-  job_launcher.submitJob( t.job, path = OUTPUT_PATH )
-  t.verifier.execute()
+  job_launcher.submitJob( t.job, path = t.output_path )
+  t.verify()
   t.print()
   return t
 
 
 
 def main():
-  os.mkdir(OUTPUT_PATH)
+  try:
+    os.mkdir(OUTPUT_PATH)
+  except:
+    pass
   
+  # test using default verifierr
   t1 = test1()
   t2 = test2()
   t3 = test3()
 
+  # test using unix-diff verifierr
   t1_ud = test1_ud()
   t2_ud = test2_ud()
+  
+  # test with staged submit/verify
+  t4 = test4()
+  t4.verify()
+  t4.print()
+  job_launcher.clean(t4.job, path = t4.output_path)
 
-  tests = [t1,t2,t3 , t1_ud,t2_ud]
+  tests = [t1,t2,t3,t4 , t1_ud,t2_ud]
   for t in tests:
-    job_launcher.clean(t.job, path = OUTPUT_PATH)
+    job_launcher.clean(t.job, path = t.output_path)
 
 
 main()
