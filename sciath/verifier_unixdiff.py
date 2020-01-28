630bbb34a5028d30815569ae832b1251631763b6
Patrick Sanan <patrick.sanan@gmail.com>
2020-01-28 17:54:04 +0100

    verifier_unixdiff.py: remove trailing whitespace

--- sciath/verifier_unixdiff.py
+++ sciath/verifier_unixdiff.py
@@ -23,21 +23,21 @@ class VerifierUnixDiff(Verifier):
           self.output_file = self.o_name[-1]
 
     def execute(self,output_path,exec_path = None):
-    
+
         status = None
         report = []
-    
+
         if not os.path.isfile(self.expected_file) :
             report.append("[UnixDiff] Expected file \"" + self.expected_file + "\" was not found")
             status = sciath_test_status.expected_file_not_found
             return
-   
+
         output_full_path = os.path.join(output_path,self.output_file)
         if not os.path.isfile(output_full_path) :
             report.append("[UnixDiff] Output file \"" + output_full_path + "\" was not found")
             status = sciath_test_status.output_file_not_found
             return
-    
+
         stdoutfile = os.path.join(output_path,"sciath.verifier-unixdiff.stdout")
         file_o = open( stdoutfile, 'w')
         ctx = subprocess.run(['diff',self.expected_file,output_full_path],stdout=file_o,stderr=subprocess.PIPE)
@@ -51,14 +51,14 @@ class VerifierUnixDiff(Verifier):
             for k in range(0,len(report)):
                 line = report[k]
                 report[k] = line.rstrip("\n")
-      
+
             report.append("[UnixDiff] Expected and output files are not identical")
             report.append("[UnixDiff] Expected file: \"" + self.expected_file + "\"")
             report.append("[UnixDiff] Output file  : \"" + self.output_file + "\"")
             status = sciath_test_status.not_ok
         else:
             status = sciath_test_status.ok
-    
+
         _remove_file(stdoutfile)
 
         return status,report
