be50cd7bc9e6ad7668536d621a4f731a0d9ce8d4
Patrick Sanan <patrick.sanan@gmail.com>
2020-02-04 17:23:30 +0100

    WIP: experiment for key-and-rel-float line verifier test

--- sciath/verifier_line.py
+++ sciath/verifier_line.py
@@ -43,3 +43,59 @@ class VerifierLine(Verifier):
 
         status = sciath_test_status.ok if passing else sciath_test_status.not_ok
         return status, report
+
+# Helper functions to generate rules
+
+# TODO this is just a proof of concept
+
+def string_get_floats(line):
+    r = []
+    re_float = re.compile('[+-]?\d+.?\d*[eE]?[+-]?\d*')
+    for word in line.split():
+        match = re.search(re_float,word)
+        if match:
+            r.append(float(match.group()))
+    return r
+
+def compare_float_values_rel(line_expected, line_out, rel_tol):
+    floats_expected = string_get_floats(line_expected.rstrip())
+    floats_out      = string_get_floats(line_out.rstrip())
+    passing = True
+    report = ''
+    for (float_expected, float_out) in zip(floats_expected, floats_out):
+        if float_expected == 0.0 and float_out != 0.0:
+            passing = False
+            report = report + 'expected value of 0.0 must be matched exactly (not checking the rest of the line)\n'
+            break
+        if abs(float_out - float_expected)/abs(float_expected) > rel_tol:
+             passing = False
+             report = report + 'expected value of ' + str(float_expected) + ' does not match output value ' + str(float_out) + ' to relative tolerance ' + str(rel_tol)
+             break
+    if passing and len(floats_expected) != len(floats_out):
+        passing = False
+        report = report + 'Wrong number of values found: ' + len(floats_out) + ' instead of ' + len(floats_expected)
+    return passing, report
+
+def float_rel_pairs_function(match_expected, match_out, rel_tol, max_err_count = 100):
+    passing = True
+    report = ''
+    err_count = 0
+    for ((lineno_expected,line_expected), (lineno_out,line_out)) in zip(match_expected.items(), match_out.items()):
+        line_passing, line_report = compare_float_values_rel(line_expected, line_out, rel_tol)
+        if not line_passing:
+            passing = False
+            report = report + '(l.' + str(lineno_expected) + '/' + str(lineno_out) + ') Lines did not match: ' + line_report
+
+            err_count = err_count + 1
+            if err_count > max_err_count:
+                report = report + 'Not checking any more lines after the first ' + max_err_count + '\n'
+    if len(match_expected) != len(match_out):
+        passing = False
+        report = report + 'Output and expected had different numbers of matches. Expected ' + len(match_expected) + ' but found ' + len(match_out) + '\n'
+    return passing, report
+
+def key_and_float_rule(key,rel_tol=1e-6):
+    rule = {}
+    rule['re'] = '^'+re.escape(key) # match on lines starting with key
+    rule['function'] = lambda e,o : float_rel_pairs_function(e,o,rel_tol=rel_tol)
+    return rule
--- tests/test/test_line_verifier.py
+++ tests/test/test_line_verifier.py
@@ -4,7 +4,7 @@ import os
 from sciath.job import Job
 from sciath.launcher import Launcher
 from sciath.test import Test
-from sciath.verifier_line import VerifierLine
+from sciath.verifier_line import VerifierLine, key_and_float_rule
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -35,17 +35,44 @@ def test2(output_path):
     t.verifier = v
     return t
 
+# Compare floats. Should pass with default tol
+def test3(output_path):
+    cmd = ['printf' , '  key 2.0 2 -3.4 1e10\nx\n\n  key 1.0\n  key  \nkey\nkey 1e-10 1.0 4.0\n  key 1e-12\n']
+    name = 'Line3'
+    t = Test(Job(cmd, name))
+    v = VerifierLine(t, expected_file = os.path.join(this_dir,"./expected/line1.expected"))
+    v.rules.append(key_and_float_rule('  key'))
+    t.verifier = v
+    return t
+
+# Compare floats. Should fail
+def test4(output_path):
+    cmd = ['printf' , '  key 7.0 2 -3.4 1e10\nx\n\n  key 1.0\n  key  \nkey\nkey 1e10 1.0 4.0\n  key 1e-12\n']
+    name = 'Line4'
+    t = Test(Job(cmd, name))
+    v = VerifierLine(t, expected_file = os.path.join(this_dir,"./expected/line1.expected"))
+    v.rules.append(key_and_float_rule('  key'))
+    t.verifier = v
+    return t
+
+# Compare floats. Should fail
+def test5(output_path):
+    cmd = ['printf' , '  key 2.0 2 -3.4 1e10\nx\n\n  key 1.0\n  key  \nkey\nkey 1e10 1.0 4.0\n  key 1e+12\n']
+    name = 'Line5'
+    t = Test(Job(cmd, name))
+    v = VerifierLine(t, expected_file = os.path.join(this_dir,"./expected/line1.expected"))
+    v.rules.append(key_and_float_rule('  key'))
+    t.verifier = v
+    return t
+
 def main():
     output_path = os.path.join(os.getcwd(),'output')
-    try:
-        os.mkdir(output_path)
-    except:
-        pass
+    os.makedirs(output_path, exist_ok = True)
 
     launcher = Launcher()
-    for t in [test1(output_path), test2(output_path)]:
-        launcher.submitJob(t.job, output_path = output_path) # only makes sense on local machine
-        status, report = t.verify(output_path = output_path)
+    for t in [test1(output_path), test2(output_path), test3(output_path), test4(output_path), test5(output_path)]:
+        launcher.submitJob(t.job, output_path = output_path)
+        status, report = t.verify(output_path = output_path) # only makes sense if submitJob blocks
         print(status)
         print(report)
 
