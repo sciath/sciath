cc761742f6bed42819b8e836042e7a86c8a57ef1
Patrick Sanan <patrick.sanan@gmail.com>
2020-06-13 13:28:54 +0200

    Self tests: make current MPI tests execute-only
    Prepare to add another group of verify-only MPI tests,
    to follow the execute-only tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,4 +2,7 @@
 # Additional arguments (e.g. --update) are passed through
 
 python -m sciath  # prompt to generate configuration file, if missing
-./minisciath/minisciath.py tests.yml $@
+./minisciath/minisciath.py --exclude-group mpi_verify tests.yml $@
+
+printf "You should run additional tests to verify MPI tests. Wait until any queued jobs are completed, and then run\n"
+printf "  ./minisciath/minisciath.py --only-group mpi_verify tests.yml\n"
