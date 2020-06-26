8befcb719df3197a9d12ee428745c25823e18380
Patrick Sanan <patrick.sanan@gmail.com>
2020-06-26 08:45:35 +0200

    task.py: de-lint

--- sciath/task.py
+++ sciath/task.py
@@ -1,4 +1,7 @@
-class Task(object):
+""" SciATH Task class """
+
+
+class Task:
     """ A class which encapsulates a command and a set of required resources
 
        A "command", here, is a list of strings, the first of which names an executable
@@ -20,7 +23,7 @@ class Task(object):
         for key, value in kwargs.items():
             self.resources[key] = value
 
-        self.setResources(**kwargs) # looking in kwargs for any resources
+        self.set_resources(**kwargs)  # looking in kwargs for any resources
         if 'mpiranks' not in self.resources:
             self.resources['mpiranks'] = 1
         if 'threads' not in self.resources:
@@ -35,7 +38,8 @@ class Task(object):
                 try:
                     self.wall_time = float(value)
                 except ValueError:
-                    message = '[SciATH error]: Cannot convert wall_time \"' + str(value) + '\" to float.'
+                    message = '[SciATH error]: Cannot convert wall_time \"' + str(
+                        value) + '\" to float.'
                     raise RuntimeError(message)
 
     def create_execute_command(self):
@@ -44,52 +48,65 @@ class Task(object):
         """
         return self.command, self.resources
 
-    def setResources(self,**kwargs):
+    def set_resources(self, **kwargs):
         """
-        Define task resources (e.g. number of mpi ranks) via "resource_name"=number keyword=value pairs.
-        The keywords used to identify the number mpi ranks is ['ranks', 'Ranks', 'mpiranks', 'MPIRanks'].
-        The keywords used to identify the number of threads is ['threads', 'ompthreads'].
+        Define task resources (e.g. number of mpi ranks)
+
+        via "resource_name"=number keyword=value pairs.
+        The keywords used to identify the number mpi ranks iare
+        ['ranks', 'Ranks', 'mpiranks', 'MPIRanks'].
+
+        The keywords used to identify the number of threads are
+        ['threads', 'ompthreads'].
+
         Unrecognized resource types will produce an error.
         """
 
         # Valid names to identify number of MPI ranks
-        ranks_k = [ 'ranks', 'Ranks', 'mpiranks', 'MPIRanks' ]
+        ranks_k = ['ranks', 'Ranks', 'mpiranks', 'MPIRanks']
         ranks_set = 0
 
         # Valid names to identify number of threads
-        threads_k = [ 'threads', 'ompthreads' ]
+        threads_k = ['threads', 'ompthreads']
         threads_set = 0
 
         # Others resources go here
 
-        # Perform error checking on non-empty dictionary. This is done to enable this method
-        # silently be called during job.__init__()
-        # Join all valid name list and check that the provided keyword is a valid resource name / identifier
+        # Perform error checking on non-empty dictionary. This is done to enable
+        # this method silently be called during job.__init__()
+        # Join all valid name list and check that the provided keyword is a
+        # valid resource name / identifier
         if len(self.resources) != 0:
-            allValidResourceNames = ranks_k + threads_k
-            for key,value in kwargs.items():
-                if key not in allValidResourceNames:
-                    message  = '[SciATH error]: Unknown resource type \"' + str(key) + '\" was requested.\n'
-                    message += '                Choose from the following: ' + " ".join(allValidResourceNames)
+            all_valid_resource_names = ranks_k + threads_k
+            for key, value in kwargs.items():
+                if key not in all_valid_resource_names:
+                    message = '[SciATH error]: Unknown resource type \"' + str(
+                        key) + '\" was requested.\n'
+                    message += '                Choose from the following: ' + " ".join(
+                        all_valid_resource_names)
                     raise RuntimeError(message)
 
         for key, value in kwargs.items():
-            if key in ranks_k: # look mpi rank keyword identifiers
-                self.resources.update({"mpiranks":int(value)})
+            if key in ranks_k:  # look mpi rank keyword identifiers
+                self.resources.update({"mpiranks": int(value)})
                 ranks_set += 1
 
-            if key in threads_k: # look for thread keyword identifiers
-                self.resources.update({"threads":int(value)})
+            if key in threads_k:  # look for thread keyword identifiers
+                self.resources.update({"threads": int(value)})
                 threads_set += 1
 
         # Sanity check that only one instance of a valid keyword
         # for a given resource type (e.g. mpi ranks) was provided
         if ranks_set > 1:
-            message  = '[SciATH error]: More than one instance of a valid MPI ranks keyword was provided to setResources().\n'
-            message += '                To set the #mpi ranks, choose one of: ' + " ".join(ranks_k)
+            message = ('[SciATH error]: More than one instance of a valid MPI '
+                       'ranks keyword was provided to set_resources().\n')
+            message += '                To set the #mpi ranks, choose one of: ' + " ".join(
+                ranks_k)
             raise RuntimeError(message)
 
         if threads_set > 1:
-            message   = '[SciATH error]: More than one instance of a valid threads keyword was provided to setResources().\n'
-            message += '                To set the #threads, choose one of: ' + " ".join(threads_k)
+            message = ('[SciATH error]: More than one instance of a valid threads'
+                       'keyword was provided to set_resources().\n')
+            message += '                To set the #threads, choose one of: ' + " ".join(
+                threads_k)
             raise RuntimeError(message)
