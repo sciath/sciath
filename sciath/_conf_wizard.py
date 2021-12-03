0a25234b8f2a66e9990202933b2e79b4a5a0733d
Patrick Sanan <patrick.sanan@gmail.com>
2021-12-03 10:30:11 +0100

    Travis CI: combine style checking jobs into single stage

--- sciath/_conf_wizard.py
+++ sciath/_conf_wizard.py
@@ -5,7 +5,11 @@ import os
 from sciath._sciath_io import py23input
 
 
-def _query_user(description="", options=None, examples=None, default=None, validate=None):
+def _query_user(description="",
+                options=None,
+                examples=None,
+                default=None,
+                validate=None):
     valid = False
     while not valid:
         options_string = "" if options is None else " <%s>" % ", ".join(options)
@@ -56,29 +60,35 @@ def _launcher_interactive_configure(launcher):
     petsc_dir = os.getenv("PETSC_DIR")
     petsc_arch = os.getenv("PETSC_ARCH")
     if petsc_dir and petsc_arch:
-        mpi_launch_examples.append("  Current PETSc MPI wrapper : %s -n <ranks>" %
-                                   os.path.join(petsc_dir, petsc_arch, "bin", "mpiexec"))
+        mpi_launch_examples.append(
+            "  Current PETSc MPI wrapper : %s -n <ranks>" %
+            os.path.join(petsc_dir, petsc_arch, "bin", "mpiexec"))
     else:
         mpi_launch_examples.append(
-            "  Example PETSc MPI wrapper : /users/myname/petsc/arch-xxx/bin/mpiexec -n <ranks>")
+            "  Example PETSc MPI wrapper : /users/myname/petsc/arch-xxx/bin/mpiexec -n <ranks>"
+        )
 
-    mpi_launch = _query_user("\n[2] MPI launch command, including <ranks> for number of ranks",
-                             examples=mpi_launch_examples,
-                             default="none",
-                             validate=launcher.is_valid_mpi_launch)
+    mpi_launch = _query_user(
+        "\n[2] MPI launch command, including <ranks> for number of ranks",
+        examples=mpi_launch_examples,
+        default="none",
+        validate=launcher.is_valid_mpi_launch)
 
     launcher.set_mpi_launch(mpi_launch)
 
     launcher.account_name = _query_user("\n[3] Account to charge", default="")
 
-    launcher.queue_name = _query_user("\n[4] Name of queue to submit tests to", default="")
+    launcher.queue_name = _query_user("\n[4] Name of queue to submit tests to",
+                                      default="")
 
-    launcher.template_filename = launcher.write_default_template(launcher.queuing_system_type)
+    launcher.template_filename = launcher.write_default_template(
+        launcher.queuing_system_type)
 
     launcher.write_definition(launcher.conf_filename)
 
     print("")
-    print("** The template for batch submission files is  %s" % launcher.template_filename)
+    print("** The template for batch submission files is  %s" %
+          launcher.template_filename)
     print("** You may modify it if desired, but it will be overwritten")
     print("** if you re-configure.")
     print("")
