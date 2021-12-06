65e59a935c3fcbdb8abb84165eff158818efeabe
Patrick Sanan <patrick.sanan@gmail.com>
2021-12-06 12:02:24 +0100

    v0.12.0

--- sciath/__init__.py
+++ sciath/__init__.py
@@ -1,7 +1,7 @@
 """ SciATH: Scientific Application Test Harness """
 from sciath._sciath_io import NamedColors
 
-__version__ = (0, 11, 0)
+__version__ = (0, 12, 0)
 
 # A default set of colors
 SCIATH_COLORS = NamedColors()
